0db3031a44949fc6e59d75e8d401ae97b4af1a1a
vctrwesley <vctrwesley@gmail.com>
2025-02-13 19:31:11 -0300

    configuracao de rotas com meu perfil e dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
+import { PainelPrincipalComponent } from './sistema/painel-principal/painel-principal.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '', component: LayoutComponent, children: []}
+  { path: '', component: LayoutComponent, children: [
+    { path: '', component: PainelPrincipalComponent }
+  ]}
 ];
 
 @NgModule({
--- src/app/sistema/sistema-routing.module.ts
+++ src/app/sistema/sistema-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from '../layout/layout.component';
+import { MeuPerfilComponent } from './meu-perfil/meu-perfil.component';
+import { PainelPrincipalComponent } from './painel-principal/painel-principal.component';
+
+const routes: Routes = [
+  { path: 'usuario', 
+    component: LayoutComponent,  
+    children: [
+      { path: 'painel-principal', component: PainelPrincipalComponent},
+      { path: 'meu-perfil', component:MeuPerfilComponent},
+    ] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
