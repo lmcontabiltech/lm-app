0b319fcb4a6fcfb9952484f1e8f8fde2bb42be30
vctrwesley <vctrwesley@gmail.com>
2025-02-13 19:18:30 -0300

    base inicial das rotas para iniciar tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', component: LayoutComponent, children: []}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
