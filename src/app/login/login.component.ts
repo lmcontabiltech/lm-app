8fe2cef069cfe2bf238ab58d6806ed96bb3b16fd
DeniseRisia <risiaresponde@gmail.com>
2025-02-17 17:59:49 -0300

    rota

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(): void {
+  
+    if (this.username && this.password) {
+    
+      this.router.navigate(['/usuario/painel-principal']);
+    } else {
+      alert('Por favor, preencha os campos corretamente.');
+    }
   }
 
 }
