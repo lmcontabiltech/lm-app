5e6ad9d671e228397fa510ff0ed4debc950f01fa
vctrwesley <vctrwesley@gmail.com>
2025-03-03 18:16:29 -0300

    base das telas de empresa e colaboradores junto de suas telas de cadastro

--- src/app/sistema/administrativo/colaboradores/colaboradores.component.ts
+++ src/app/sistema/administrativo/colaboradores/colaboradores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-colaboradores',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColaboradoresComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  cadastrarColaborador(): void {
+    this.router.navigate(['/usuario/cadastro-de-colaborador']); 
   }
 
 }
--- src/app/sistema/administrativo/empresas/empresas.component.ts
+++ src/app/sistema/administrativo/empresas/empresas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-empresas',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpresasComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  cadastrarEmpresa(): void {
+    this.router.navigate(['/usuario/cadastro-de-empresa']); 
   }
 
 }
