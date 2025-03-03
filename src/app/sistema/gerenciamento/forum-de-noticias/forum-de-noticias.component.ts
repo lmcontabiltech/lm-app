88d6f74401da1af6a4e8ff0a0735632cf93080d6
vctrwesley <vctrwesley@gmail.com>
2025-03-03 14:09:58 -0300

    adicionando componentes de acordo com figma e suas rotas

---
+++ src/app/sistema/gerenciamento/forum-de-noticias/forum-de-noticias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forum',
+  templateUrl: './forum-de-noticias.component.html',
+  styleUrls: ['./forum-de-noticias.component.css']
+})
+export class ForumDeNoticiasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/sistema/sistema.module.ts
+++ src/app/sistema/sistema.module.ts
@@ -5,17 +5,32 @@ import { SistemaRoutingModule } from './sistema-routing.module';
 import { PainelPrincipalComponent } from './painel-principal/painel-principal.component';
 import { MeuPerfilComponent } from './meu-perfil/meu-perfil.component';
 import { FormsModule } from '@angular/forms';
-
+import { NotificacoesComponent } from './notificacoes/notificacoes.component';
+import { EmpresasComponent } from './administrativo/empresas/empresas.component';
+import { ColaboradoresComponent } from './administrativo/colaboradores/colaboradores.component';
+import { ForumDeNoticiasComponent } from './gerenciamento/forum-de-noticias/forum-de-noticias.component';
+import { ChatComponent } from './gerenciamento/chat/chat.component';
+import { AtividadesComponent } from './gerenciamento/atividades/atividades.component';
+import { CadastroDeColaboradorComponent } from './administrativo/cadastro-de-colaborador/cadastro-de-colaborador.component';
+import { CadastroDeEmpresaComponent } from './administrativo/cadastro-de-empresa/cadastro-de-empresa.component';
+import { CadastroDeNoticiaComponent } from './gerenciamento/cadastro-de-noticia/cadastro-de-noticia.component';
+import { CadastroDeAtividadeComponent } from './gerenciamento/cadastro-de-atividade/cadastro-de-atividade.component';
 
 @NgModule({
   declarations: [
     PainelPrincipalComponent,
-    MeuPerfilComponent
+    MeuPerfilComponent,
+    NotificacoesComponent,
+    EmpresasComponent,
+    ColaboradoresComponent,
+    ForumDeNoticiasComponent,
+    ChatComponent,
+    AtividadesComponent,
+    CadastroDeColaboradorComponent,
+    CadastroDeEmpresaComponent,
+    CadastroDeNoticiaComponent,
+    CadastroDeAtividadeComponent,
   ],
-  imports: [
-    CommonModule,
-    SistemaRoutingModule,
-    FormsModule
-  ]
+  imports: [CommonModule, SistemaRoutingModule, FormsModule],
 })
-export class SistemaModule { }
+export class SistemaModule {}
