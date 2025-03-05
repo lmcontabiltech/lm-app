2b1d2dd18a9b105f1813d7a324417d3ad7e5bb49
vctrwesley <vctrwesley@gmail.com>
2025-03-05 17:21:57 -0300

    base do layout do forum de noticias

--- src/app/sistema/gerenciamento/forum-de-noticia/forum-de-noticia.component.ts
+++ src/app/sistema/gerenciamento/forum-de-noticia/forum-de-noticia.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forum-de-noticia',
   templateUrl: './forum-de-noticia.component.html',
-  styleUrls: ['./forum-de-noticia.component.css']
+  styleUrls: ['./forum-de-noticia.component.css'],
 })
 export class ForumDeNoticiaComponent implements OnInit {
+  constructor(
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  cadastrarNoticia(): void {
+    this.router.navigate(['/usuario/cadastro-de-noticia']);
   }
 
+  onSearch(searchTerm: string) {
+    console.log('Search term:', searchTerm);
+  }
 }
