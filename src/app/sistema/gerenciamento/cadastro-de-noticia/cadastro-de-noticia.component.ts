c1459b6c8c9c902d854b5907c7e7cccd63d9f6de
vctrwesley <vctrwesley@gmail.com>
2025-03-05 17:28:59 -0300

    cadastro de noticias

--- src/app/sistema/gerenciamento/cadastro-de-noticia/cadastro-de-noticia.component.ts
+++ src/app/sistema/gerenciamento/cadastro-de-noticia/cadastro-de-noticia.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro-de-noticia',
   templateUrl: './cadastro-de-noticia.component.html',
-  styleUrls: ['./cadastro-de-noticia.component.css']
+  styleUrls: ['./cadastro-de-noticia.component.css'],
 })
 export class CadastroDeNoticiaComponent implements OnInit {
+  constructor(
+    private location: Location,
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goBack() {
+    this.location.back();
   }
-
 }
