528f818d85839bac5b193f714a91a69cc5e3d3cb
vctrwesley <vctrwesley@gmail.com>
2025-03-13 19:07:24 -0300

    base inicial do modal e testes

--- src/app/shared/modal-geral/modal-geral.component.ts
+++ src/app/shared/modal-geral/modal-geral.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal-geral',
   templateUrl: './modal-geral.component.html',
-  styleUrls: ['./modal-geral.component.css']
+  styleUrls: ['./modal-geral.component.css'],
 })
-export class ModalGeralComponent implements OnInit {
+export class ModalGeralComponent {
+  @Input() show: boolean = false;
+  @Input() title: string = 'Modal';
+  @Input() size: string = 'xl:max-w-7xl';
+  @Input() footer: boolean = true;
 
-  constructor() { }
+  @Output() closeModal = new EventEmitter<void>();
+  @Output() saveModal = new EventEmitter<void>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  handleSave(): void {
+    this.saveModal.emit();
   }
 
+  onModalClose(): void {
+    this.closeModal.emit();
+  }
 }
