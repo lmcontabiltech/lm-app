8f714e07672cb3633eeaab418e970ef1f261486d
vctrwesley <vctrwesley@gmail.com>
2025-04-19 20:55:01 -0300

    melhorndo a reposta do select com o form reativo

--- src/app/shared/select-padrao/select-padrao.component.ts
+++ src/app/shared/select-padrao/select-padrao.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit, Input, HostListener, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, HostListener, Output, EventEmitter, ElementRef, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-select-padrao',
   templateUrl: './select-padrao.component.html',
   styleUrls: ['./select-padrao.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectPadraoComponent),
+      multi: true,
+    },
+  ],
 })
 export class SelectPadraoComponent {
   @Input() label: string = '';
@@ -14,7 +22,22 @@ export class SelectPadraoComponent {
 
   isOpen: boolean = false;
 
+  private onChange: (value: any) => void = () => {};
+  private onTouched: () => void = () => {};
+
   constructor(private elementRef: ElementRef) {}
+
+  writeValue(value: any): void {
+    this.selectedValue = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 
   onSelect(option: { value: string, description: string }) {
     this.selectedValue = option.value;
