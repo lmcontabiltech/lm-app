d6f82ae314f9b5a78d3cc20ec0cf74a314b27a58
vctrwesley <vctrwesley@gmail.com>
2025-04-19 21:54:21 -0300

    GET, PUT e DELETE de processos feitos no service e melhoria nas mensagens de erro

--- src/app/services/gerenciamento/processo.service.ts
+++ src/app/services/gerenciamento/processo.service.ts
@@ -17,7 +17,77 @@ export class ProcessoService {
     return this.http.post<Processo>(this.apiURL, processo).pipe(
       map((response) => response),
       catchError((error) => {
-        let errorMessage = 'Erro ao cadastrar a processo.';
+        let errorMessage = 'Erro ao cadastrar o processo.';
+
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro: ${error.error.message}`;
+        } else if (error.status) {
+          errorMessage = `Erro no servidor: ${error.status} - ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
+
+  getProcessos(): Observable<Processo[]> {
+    return this.http.get<Processo[]>(this.apiURL).pipe(
+      map((response) => response),
+      catchError((error) => {
+        let errorMessage = 'Erro ao buscar os processos.';
+
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro: ${error.error.message}`;
+        } else if (error.status) {
+          errorMessage = `Erro no servidor: ${error.status} - ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
+
+  getProcessoById(id: string): Observable<Processo> {
+    const url = `${this.apiURL}/${id}`;
+    return this.http.get<Processo>(url).pipe(
+      map((response) => response),
+      catchError((error) => {
+        let errorMessage = 'Erro ao buscar o processo.';
+
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro: ${error.error.message}`;
+        } else if (error.status) {
+          errorMessage = `Erro no servidor: ${error.status} - ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
+
+  deletarProcesso(id: string): Observable<void> {
+    const url = `${this.apiURL}/${id}`;
+    return this.http.delete<void>(url).pipe(
+      catchError((error) => {
+        let errorMessage = 'Erro ao deletar o processo.';
+
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro: ${error.error.message}`;
+        } else if (error.status) {
+          errorMessage = `Erro no servidor: ${error.status} - ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
+
+  atualizarProcesso(id: string, processo: Processo): Observable<Processo> {
+    const url = `${this.apiURL}/${id}`;
+    return this.http.put<Processo>(url, processo).pipe(
+      map((response) => response),
+      catchError((error) => {
+        let errorMessage = 'Erro ao atualizar o processo.';
 
         if (error.error instanceof ErrorEvent) {
           errorMessage = `Erro: ${error.error.message}`;
