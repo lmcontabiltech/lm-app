2044fa3a6fa03d1df0fd9486d45150bc66beaf07
vctrwesley <vctrwesley@gmail.com>
2025-05-30 15:59:23 -0300

    Adicionando componente DetalhesFluxo com estrutura de layout, incluindo lógica de navegação para voltar à página anterior.

--- src/app/sistema/gerenciamento/visualizar-gerenciamento/detalhes-fluxo/detalhes-fluxo.component.ts
+++ src/app/sistema/gerenciamento/visualizar-gerenciamento/detalhes-fluxo/detalhes-fluxo.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhes-fluxo',
   templateUrl: './detalhes-fluxo.component.html',
-  styleUrls: ['./detalhes-fluxo.component.css']
+  styleUrls: ['./detalhes-fluxo.component.css'],
 })
 export class DetalhesFluxoComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goBack() {
+    this.location.back();
   }
-
 }
