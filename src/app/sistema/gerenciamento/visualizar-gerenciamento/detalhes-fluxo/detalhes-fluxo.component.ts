354bf1580cf5ad67e6ae5c63bdef398dc4feff8a
vctrwesley <vctrwesley@gmail.com>
2025-05-30 15:33:17 -0300

    Adicionando componentes Fluxos, HistoricoAtividades e DetalhesFluxo com seus respectivos templates, estilos e testes.

---
+++ src/app/sistema/gerenciamento/visualizar-gerenciamento/detalhes-fluxo/detalhes-fluxo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-fluxo',
+  templateUrl: './detalhes-fluxo.component.html',
+  styleUrls: ['./detalhes-fluxo.component.css']
+})
+export class DetalhesFluxoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
