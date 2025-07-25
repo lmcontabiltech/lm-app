40ffbe37b890bf030d44bca69d1d41c74cd68583
vctrwesley <vctrwesley@gmail.com>
2025-07-25 00:03:09 -0300

    feat: Implementando serviço e métodos para resumo de atividades do colaborador no dashboard

---
+++ src/app/services/graficos/dashboard-colaborador.service.ts
@@ -0,0 +1,69 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { DashboardAtividadesPorSetorResponseDTO } from 'src/app/sistema/dashboards/dashboard-admin/atividades-por-setor';
+import { Setor } from 'src/app/sistema/administrativo/cadastro-de-colaborador/setor';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardColaboradorService {
+  apiURL: string = environment.apiURLBase + '/api/grafico';
+
+  constructor(private http: HttpClient) {}
+
+  getAtividadesResumoSetor(
+    setor: Setor,
+    dataInicio: string
+  ): Observable<DashboardAtividadesPorSetorResponseDTO> {
+    const url = `${this.apiURL}/atividades/resumo`;
+    const params = {
+      setor: setor,
+      dataInicio: dataInicio,
+    };
+
+    return this.http
+      .get<DashboardAtividadesPorSetorResponseDTO>(url, { params })
+      .pipe(
+        map((response) => response),
+        catchError((error) => {
+          let errorMessage = `Erro ao buscar resumo de atividades para o setor ${setor}.`;
+          if (error.error instanceof ErrorEvent) {
+            errorMessage = `Erro: ${error.error.message}`;
+          } else if (error.status) {
+            errorMessage = `Erro no servidor: ${error.status} - ${error.message}`;
+          }
+          console.error(errorMessage);
+          return throwError(() => new Error(errorMessage));
+        })
+      );
+  }
+
+  getResumoAtividadesUsuario(): Observable<{
+    concluidas: number;
+    emAndamento: number;
+    totalAtribuidas: number;
+  }> {
+    const url = `${this.apiURL}/usuarios/resumo`;
+    return this.http
+      .get<{
+        concluidas: number;
+        emAndamento: number;
+        totalAtribuidas: number;
+      }>(url)
+      .pipe(
+        catchError((error) => {
+          let errorMessage = 'Erro ao buscar resumo das atividades do usuário.';
+          if (error.error instanceof ErrorEvent) {
+            errorMessage = `Erro: ${error.error.message}`;
+          } else if (error.status) {
+            errorMessage = `Erro no servidor: ${error.status} - ${error.message}`;
+          }
+          console.error(errorMessage);
+          return throwError(() => new Error(errorMessage));
+        })
+      );
+  }
+}
