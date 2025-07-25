2c1cd759cd0a2c59b30f6c2915c9bfa1978ab098
DeniseRisia <risiaresponde@gmail.com>
2025-07-25 17:02:29 -0300

    correcao estag

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -31,6 +31,7 @@ export class AuthGuard implements CanActivate {
     if (
       role === 'ROLE_ADMIN' ||
       role === 'ROLE_COORDENADOR' ||
+      role === 'ROLE_ESTAGIARIO' ||
       role === 'ROLE_USER'
     ) {
       return true;
