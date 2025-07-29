b558657105e9df35ce161f9006e3ccff99377a61
vctrwesley <vctrwesley@gmail.com>
2025-07-29 03:25:42 -0300

    refactor: ajuste em periferico e na porta do localhost para a API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiURLBase: 'http://localhost:8088',
+  // apiURLBase: 'http://localhost:8087',
   apiURLBase: 'https://backend-app-w53w.onrender.com',
   clientId: 'lm-app',
   clientSecret: '@2026',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ interface Environment {
 
 export const environment: Environment = {
   production: false,
-  //apiURLBase: 'http://localhost:8088',
+  // apiURLBase: 'http://localhost:8087',
   apiURLBase: 'https://backend-app-w53w.onrender.com',
   clientId: 'lm-app',
   clientSecret: '@2026',
