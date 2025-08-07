660955c076225e3796db1cc85b9d0f39fe11a0bc
vctrwesley <vctrwesley@gmail.com>
2025-08-07 13:53:56 -0300

    feat: atualizando tela do forum de noticias

--- src/app/sistema/gerenciamento/forum-de-noticia/forum-de-noticia.component.ts
+++ src/app/sistema/gerenciamento/forum-de-noticia/forum-de-noticia.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Noticia } from './noticia';
 import { Setor } from '../../administrativo/cadastro-de-colaborador/setor';
+import { SetorDescricao } from '../../administrativo/cadastro-de-colaborador/setor-descricao';
 
 @Component({
   selector: 'app-forum-de-noticia',
@@ -10,24 +11,63 @@ import { Setor } from '../../administrativo/cadastro-de-colaborador/setor';
 })
 export class ForumDeNoticiaComponent implements OnInit {
   noticias: Noticia[] = [
-      { titulo: 'Título da notícia', data: '22/12/2024 às 12:30', setor: Setor.FISCAL },
-      { titulo: 'Título da notícia', data: '22/12/2024 às 12:30', setor: Setor.PESSOAL },
-      { titulo: 'Título da notícia', data: '22/12/2024 às 12:30', setor: Setor.CONTABIL },
-      { titulo: 'Título da notícia', data: '22/12/2024 às 12:30', setor: Setor.FISCAL },
-      { titulo: 'Título da notícia', data: '22/12/2024 às 12:30', setor: 'ALL' },
-      { titulo: 'Título da notícia', data: '22/12/2024 às 12:30', setor: Setor.CONTABIL },
-      { titulo: 'Título da notícia', data: '22/12/2024 às 12:30', setor: Setor.PESSOAL },
-      { titulo: 'Título da notícia', data: '22/12/2024 às 12:30', setor: Setor.FISCAL }
-    ];
+    {
+      titulo: 'Título da notícia',
+      data: '22/12/2024 às 12:30',
+      setor: Setor.FISCAL,
+    },
+    {
+      titulo: 'Título da notícia',
+      data: '22/12/2024 às 12:30',
+      setor: Setor.PESSOAL,
+    },
+    {
+      titulo: 'Título da notícia',
+      data: '22/12/2024 às 12:30',
+      setor: Setor.CONTABIL,
+    },
+    {
+      titulo: 'Título da notícia',
+      data: '22/12/2024 às 12:30',
+      setor: Setor.FISCAL,
+    },
+    { titulo: 'Título da notícia', data: '22/12/2024 às 12:30', setor: 'ALL' },
+    {
+      titulo: 'Título da notícia',
+      data: '22/12/2024 às 12:30',
+      setor: Setor.CONTABIL,
+    },
+    {
+      titulo: 'Título da notícia',
+      data: '22/12/2024 às 12:30',
+      setor: Setor.PESSOAL,
+    },
+    {
+      titulo: 'Título da notícia',
+      data: '22/12/2024 às 12:30',
+      setor: Setor.FISCAL,
+    },
+  ];
 
-    itensPorPagina = 5;
-    paginaAtual = 1;
-    totalPaginas = Math.ceil(this.noticias.length / this.itensPorPagina);
-    noticiasPaginados: Noticia[] = [];
+  itensPorPagina = 5;
+  paginaAtual = 1;
+  totalPaginas = Math.ceil(this.noticias.length / this.itensPorPagina);
+  noticiasPaginados: Noticia[] = [];
 
-  constructor(
-    private router: Router
-  ) {}
+  termoBusca: string = '';
+  mensagemBusca: string = '';
+  isLoading = false;
+  successMessage: string = '';
+  messageTimeout: any;
+
+  setores = Object.keys(Setor).map((key) => ({
+    value: Setor[key as keyof typeof Setor],
+    description: SetorDescricao[Setor[key as keyof typeof Setor]],
+  }));
+
+  selectedSetor: string = '';
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.atualizarPaginacao();
@@ -47,22 +87,32 @@ export class ForumDeNoticiaComponent implements OnInit {
     this.noticiasPaginados = this.noticias.slice(inicio, fim);
   }
 
-  mudarPagina(pagina: number): void {
-    this.paginaAtual = pagina;
+  get totalItens() {
+    return this.noticias.length;
+  }
+
+  onPaginaMudou(novaPagina: number) {
+    this.paginaAtual = novaPagina;
     this.atualizarPaginacao();
   }
 
-  paginaAnterior(): void {
-    if (this.paginaAtual > 1) {
-      this.paginaAtual--;
-      this.atualizarPaginacao();
+  exibirMensagemDeSucesso(): void {
+    const state = window.history.state as { successMessage?: string };
+    if (state?.successMessage) {
+      this.successMessage = state.successMessage;
+      setTimeout(() => (this.successMessage = ''), 3000);
+      window.history.replaceState({}, document.title);
     }
   }
 
-  proximaPagina(): void {
-    if (this.paginaAtual < this.totalPaginas) {
-      this.paginaAtual++;
-      this.atualizarPaginacao();
-    }
+  showMessage(type: 'success' | 'error', msg: string) {
+    this.clearMessage();
+    if (type === 'success') this.successMessage = msg;
+    this.messageTimeout = setTimeout(() => this.clearMessage(), 3000);
+  }
+
+  clearMessage() {
+    this.successMessage = '';
+    if (this.messageTimeout) clearTimeout(this.messageTimeout);
   }
 }
