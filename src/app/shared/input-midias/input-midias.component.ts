088012356876ee6d548f17bf06f4ec2c4e510e9c
vctrwesley <vctrwesley@gmail.com>
2025-08-11 15:42:16 -0300

    feat: adicionando componentes InputWord e InputMidias

---
+++ src/app/shared/input-midias/input-midias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-midias',
+  templateUrl: './input-midias.component.html',
+  styleUrls: ['./input-midias.component.css']
+})
+export class InputMidiasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/input-word/input-word.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-word',
+  templateUrl: './input-word.component.html',
+  styleUrls: ['./input-word.component.css']
+})
+export class InputWordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
