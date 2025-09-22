7311fef020968add70d1fa529c3daae6270a4e36
vctrwesley <vctrwesley@gmail.com>
2025-09-22 15:40:03 -0300

    feat: ajustando menu de botões e layout novo do scanner

---
+++ src/app/services/gerenciamento/scanner.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DocumentosService {
+  apiURL: string = environment.apiURLBase + '/scanner';
+
+  constructor(private http: HttpClient) {}
+}
