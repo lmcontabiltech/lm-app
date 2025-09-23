c6143832cc8ac9686b35cb9e23516e7677c820f5
vctrwesley <vctrwesley@gmail.com>
2025-09-23 14:32:23 -0300

    organizando componentes de modal e ajustando suas importações

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputArquivosComponent } from './input-arquivos/input-arquivos.component';
 import { InputImagensComponent } from './input-imagens/input-imagens.component';
-import { ModalDeleteComponent } from './modal-delete/modal-delete.component';
+import { ModalDeleteComponent } from './modal/modal-delete/modal-delete.component';
 import { MultiploSelectComponent } from './multiplo-select/multiplo-select.component';
 import { SelectPadraoComponent } from './select-padrao/select-padrao.component';
 import { SearchComponent } from './search/search.component';
@@ -10,7 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { SetorTagComponent } from './setor-tag/setor-tag.component';
 import { InputPlusComponent } from './input-plus/input-plus.component';
 import { CardAtvComponent } from './card-atv/card-atv.component';
-import { ModalAtividadeComponent } from './modal-atividade/modal-atividade.component';
+import { ModalAtividadeComponent } from './modal/modal-atividade/modal-atividade.component';
 import { FiltroAtividadesComponent } from './filtro-atividades/filtro-atividades.component';
 import { PaginationComponent } from './pagination/pagination.component';
 import { LoadingComponent } from './loading/loading.component';
@@ -22,13 +22,14 @@ import { GraficoLinhaComponent } from './graficos/grafico-linha/grafico-linha.co
 import { GraficoBarraVerticalComponent } from './graficos/grafico-barra-vertical/grafico-barra-vertical.component';
 import { GraficoRosquinhaComponent } from './graficos/grafico-rosquinha/grafico-rosquinha.component';
 import { FeedbackComponent } from './feedback/feedback.component';
-import { ModalPadraoComponent } from './modal-padrao/modal-padrao.component';
+import { ModalPadraoComponent } from './modal/modal-padrao/modal-padrao.component';
 import { BotTiComponent } from './suporte/bot-ti/bot-ti.component';
 import { InputWordComponent } from './input-word/input-word.component';
 import { InputMidiasComponent } from './input-midias/input-midias.component';
 import { MensalComponent } from './agenda/mensal/mensal.component';
 import { SemanalComponent } from './agenda/semanal/semanal.component';
 import { AnualComponent } from './agenda/anual/anual.component';
+import { ModalCadastroComponent } from './modal/modal-cadastro/modal-cadastro.component';
 
 export function playerFactory() {
   return player;
@@ -62,6 +63,7 @@ export function playerFactory() {
     MensalComponent,
     SemanalComponent,
     AnualComponent,
+    ModalCadastroComponent,
   ],
   imports: [
     CommonModule,
@@ -95,6 +97,7 @@ export function playerFactory() {
     MensalComponent,
     SemanalComponent,
     AnualComponent,
+    ModalCadastroComponent
   ],
 })
 export class SharedModule {}
